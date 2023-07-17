170dae3a4b78d523a6d974a8f2096f43c30c25ef
johnkagiri <kagirijohn@gmail.com>
2023-07-17 19:57:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
+function titleCased(){
+// Function to convert a string to title case
+const toTitleCase = (str) => {
+  return str
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+};
 
-const titleCased = () => {
-  return tutorials
+// Applying the toTitleCase function to each tutorial title using map()
+const formattedTutorials = tutorials.map((tutorial) => toTitleCase(tutorial));
+return formattedTutorials;
 }
+console.log(titleCased());
